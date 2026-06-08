3f4d65f4a31a22e3bdca2abc63690a4530ff403a
Gabriella Lima <gabriellalimaalb@gmail.com>
2026-06-08 12:29:12 -0300

    outras abas

---
+++ OneDrive/Documentos/portifolio-ic/script.js
@@ -0,0 +1,15 @@
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.botao_sociais')) {
+        var dropdowns = document.getElementsByClassName("links");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
